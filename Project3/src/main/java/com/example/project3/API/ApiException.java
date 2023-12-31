38ea05a49e765ab17d24d6e2e92759e2b900686e
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-12-31 13:22:48 +0300

    Delete Project3 directory

--- Project3/src/main/java/com/example/project3/API/ApiException.java
+++
@@ -1,7 +0,0 @@
-package com.example.project3.API;
-
-public class ApiException extends   RuntimeException{
-    public ApiException(String message){
-        super(message);
-    }
-}
--- Project3/src/main/java/com/example/project3/Service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.project3.Service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class EmployeeService {
-}
